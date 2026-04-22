717c0c456273d1d44f39db1e9d5c83ee628223f4
wender.silva <wender.silva@CANT-008>
2026-04-22 14:32:55 -0300

    Primeiro Comit

---
+++ SaidaDeValoresTabuada/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Tabuada do 9");
+Console.WriteLine("9 * 1 = " + (9 * 1));
+Console.WriteLine("9 * 2 = " + (9 * 2));
+Console.WriteLine("9 * 2 = " + (9 * 3));
+Console.WriteLine("9 * 2 = " + (9 * 4));
+Console.WriteLine("9 * 2 = " + (9 * 5));
+Console.WriteLine("9 * 2 = " + (9 * 6));
+Console.WriteLine("9 * 2 = " + (9 * 7));
+Console.WriteLine("9 * 2 = " + (9 * 8));
+Console.WriteLine("9 * 2 = " + (9 * 9));
